650d4a5abfb9ef1601b4e7829b997bc71d22011d
Djair Soares <djhanlj@gmail.com>
2022-08-06 22:19:49 -0300

    validacao do e-mail com criacao de tratamento de erros

--- src/main/java/com/zup/mercadolivre/novousuario/NovoUsuarioRequest.java
+++ src/main/java/com/zup/mercadolivre/novousuario/NovoUsuarioRequest.java
@@ -1,6 +1,7 @@
 package com.zup.mercadolivre.novousuario;
 
 import com.zup.mercadolivre.model.Usuario;
+import com.zup.mercadolivre.beanvalidation.UniqueValue;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
@@ -10,10 +11,10 @@ public class NovoUsuarioRequest {
 
     @Email
     @NotBlank
-    private String email;
+    private final String email;
     @NotBlank
     @Length( min = 6)
-    private String senha;
+    private final String senha;
 
     public NovoUsuarioRequest(@Email @NotBlank String email,
                               @NotBlank @Length( min = 6) String senha) {
@@ -24,5 +25,9 @@ public class NovoUsuarioRequest {
 
     public Usuario toModel(){
         return new Usuario(this.email, new SenhaLimpa(senha));
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
